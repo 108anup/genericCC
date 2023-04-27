d244c048b4550d5d852ef0b9dba978acae2bd4cc
Anup Agarwal <anupa@andrew.cmu.edu>
2023-04-26 20:19:39 -0400

    Checkpoint.

--- slow_conv.cc
+++ slow_conv.cc
@@ -1,72 +1,74 @@
 #include "slow_conv.hh"
 
 void SlowConv::init() {
-    _intersend_time = 0;
-    _the_window = 2;
+	_intersend_time = 0;
+	_the_window = 2;
 }
 
 SeqNumDelta SlowConv::count_loss(SeqNum seq) {
 	SeqNumDelta segs_lost = 0;
 	for (auto it = unacknowledged_segs.begin(); it != unacknowledged_segs.end();
 		 it++) {
-		if(it->first < seq) {
-            segs_lost++;
-        }
+		if (it->first < seq) {
+			segs_lost++;
+		}
 	}
 	return segs_lost;
 }
 
-void SlowConv::onACK(SeqNum ack, Time receiver_timestamp, Time sender_timestamp) {
-    SeqNum seq = ack - 1;
+void SlowConv::onACK(SeqNum ack, Time receiver_timestamp,
+					 Time sender_timestamp) {
+	SeqNum seq = ack - 1;
 
-    if(unacknowledged_segs.count(seq) == 0) {std::cerr<<"Unknown Ack!! "<<seq<<std::endl; return;}
-    if(unacknowledged_segs.count(seq) > 1) {std::cerr<<"Dupsent!! "<<seq<<std::endl; return;}
-    assert (unacknowledged_segs.count(seq) == 1);
+	if (unacknowledged_segs.count(seq) == 0) {
+		std::cerr << "Unknown Ack!! " << seq << std::endl;
+		return;
+	}
+	if (unacknowledged_segs.count(seq) > 1) {
+		std::cerr << "Dupsent!! " << seq << std::endl;
+		return;
+	}
+	assert(unacknowledged_segs.count(seq) == 1);
 
 	SegmentData seg = unacknowledged_segs[seq];
 	Time sent_time = seg.send_tstamp;
 	unacknowledged_segs.erase(seq);
 
-    SeqNumDelta segs_lost = count_loss(seq);
-    seg.this_loss_count = segs_lost;
+	SeqNumDelta segs_lost = count_loss(seq);
+	seg.this_loss_count = segs_lost;
 
-    Time now = current_timestamp();
+	Time now = current_timestamp();
 	seg.rtt = now - sent_time;
 
-    cum_segs_lost++;
-    cum_segs_delivered++;
+	cum_segs_lost++;
+	cum_segs_delivered++;
 
 	update_history(now, seg);  // this calls update beliefs
 }
 
-void SlowConv::onPktSent(SeqNum seq_num) {
-    cum_segs_sent++;
-}
+void SlowConv::onPktSent(SeqNum seq_num) { cum_segs_sent++; }
 
-void SlowConv::update_beliefs_minc_maxc(Time now) {
-
-}
+void SlowConv::update_beliefs_minc_maxc(Time now) {}
 
 void SlowConv::update_beliefs_minc_lambda(Time now) {
-
-    if(history.size() <= 1) {
+	if (history.size() <= 1) {
 		return;
 	}
 
-    TimeDelta rtprop = beliefs.min_rtt;
-    TimeDelta jitter = beliefs.min_rtt * JITTER_MULTIPLIER;
+	TimeDelta rtprop = beliefs.min_rtt;
+	TimeDelta jitter = beliefs.min_rtt * JITTER_MULTIPLIER;
 
-    bool this_high_delay;
-    bool this_loss;
-    bool this_underutilized;
-    bool cum_underutilized;
+	bool this_high_delay;
+	bool this_loss;
+	bool this_underutilized;
+	bool cum_underutilized;
 
-    History &latest = history.back();
+	History &latest = history.back();
 	SeqNum delivered_1rtt_ago = latest.creation_cum_segs_delivered_at_send;
 
-    this_high_delay = latest.interval_max_rtt > rtprop + jitter;
-    this_loss = latest.interval_segs_lost > 0;
-    this_underutilized = !this_high_delay && !this_loss;
+	this_high_delay = latest.interval_max_rtt > rtprop + jitter;
+	this_loss = latest.interval_segs_lost > 0;
+	this_underutilized = !this_high_delay && !this_loss;
 	cum_underutilized = this_underutilized;
 
 	SegsRate new_minc_lambda = INIT_MIN_C;
@@ -76,20 +78,20 @@ void SlowConv::update_beliefs_minc_lambda(Time now) {
 		History &st = history[hid];
 
 		this_high_delay = st.interval_max_rtt > rtprop + jitter;
-        this_loss = st.interval_segs_lost > 0;
-        this_underutilized = !this_high_delay && !this_loss;
-        cum_underutilized = cum_underutilized && this_underutilized;
+		this_loss = st.interval_segs_lost > 0;
+		this_underutilized = !this_high_delay && !this_loss;
+		cum_underutilized = cum_underutilized && this_underutilized;
 
 		if (past < MEASUREMENT_INTERVAL_HISTORY) continue;
 
 		History &et = history[hid + MEASUREMENT_INTERVAL_HISTORY];
 
-        if(et.creation_cum_segs_delivered_at_send > delivered_1rtt_ago)
+		if (et.creation_cum_segs_delivered_at_send > delivered_1rtt_ago)
 			continue;
 
-        st.processed = true;
+		st.processed = true;
 
-        if(!cum_underutilized) break;
+		if (!cum_underutilized) break;
 
 		SeqNumDelta this_segs_sent =
 			et.creation_cum_sent_segs - st.creation_cum_sent_segs;
@@ -99,23 +101,25 @@ void SlowConv::update_beliefs_minc_lambda(Time now) {
 	}
 
 	beliefs.min_c_lambda = std::max(beliefs.min_c_lambda, new_minc_lambda);
-    // TODO: Implement timeout
+	// TODO: Implement timeout
 }
 
-void SlowConv::update_beliefs(Time now, SegmentData seg,
-							  bool updated_history,
+void SlowConv::update_beliefs(Time now, SegmentData seg, bool updated_history,
 							  TimeDelta time_since_last_update) {
 	beliefs.min_rtt = std::min(beliefs.min_rtt, seg.rtt);
-    TimeDelta jitter = beliefs.min_rtt * JITTER_MULTIPLIER;
+	TimeDelta jitter = beliefs.min_rtt * JITTER_MULTIPLIER;
 
-    beliefs.min_qdel = std::max((TimeDelta) 0, seg.rtt - beliefs.min_rtt - jitter);
-    beliefs.bq_belief1 = cum_segs_sent - cum_segs_delivered - cum_segs_lost;
+	beliefs.min_qdel =
+		std::max((TimeDelta)0, seg.rtt - beliefs.min_rtt - jitter);
+	beliefs.bq_belief1 = cum_segs_sent - cum_segs_delivered - cum_segs_lost;
 
-    if(updated_history) {
-        SeqNumDelta estimated_sent = cum_segs_sent - beliefs.last_segs_sent;
-        SeqNumDelta estimated_delivered = beliefs.min_c_lambda * time_since_last_update;
-        beliefs.bq_belief2 = beliefs.bq_belief2 + estimated_sent - estimated_delivered;
-        beliefs.last_segs_sent = cum_segs_sent;
+	if (updated_history) {
+		SeqNumDelta estimated_sent = cum_segs_sent - beliefs.last_segs_sent;
+		SeqNumDelta estimated_delivered =
+			beliefs.min_c_lambda * time_since_last_update;
+		beliefs.bq_belief2 =
+			beliefs.bq_belief2 + estimated_sent - estimated_delivered;
+		beliefs.last_segs_sent = cum_segs_sent;
 
 		update_beliefs_minc_maxc(now);
 		update_beliefs_minc_lambda(now);
@@ -127,9 +131,10 @@ void SlowConv::update_history(Time now, SegmentData seg) {
 	TimeDelta time_since_last_update = now - last_history_update_time;
 	if (time_since_last_update >= inter_history_time) {
 		last_history_update_time = now;
-		History h = History({now, seg.rtt, seg.rtt, cum_segs_sent,
-							 cum_segs_delivered, cum_segs_lost, sending_rate,
-							 seg.cum_segs_delivered_at_send, seg.this_loss_count});
+		History h =
+			History({now, seg.rtt, seg.rtt, cum_segs_sent, cum_segs_delivered,
+					 cum_segs_lost, sending_rate,
+					 seg.cum_segs_delivered_at_send, seg.this_loss_count});
 		history.push_back(h);
 		update_beliefs(now, seg, true, time_since_last_update);
 	} else {
@@ -140,6 +145,4 @@ void SlowConv::update_history(Time now, SegmentData seg) {
 	}
 }
 
-void SlowConv::set_rate_cwnd() {
-
-}
+void SlowConv::set_rate_cwnd() {}
--- slow_conv.hh
+++ slow_conv.hh
@@ -1,29 +1,30 @@
-#include <unordered_map>
 #include <boost/circular_buffer.hpp>
+#include <unordered_map>
 
 #include "ccc.hh"
 
-typedef double Time;  // ms (cumulative)
-typedef double TimeDelta;  // ms
-typedef int32_t SeqNum;  // Segments (Segs)
+typedef double Time;		  // ms (cumulative)
+typedef double TimeDelta;	  // ms
+typedef int32_t SeqNum;		  // Segments (Segs)
 typedef int32_t SeqNumDelta;  // Segs (cuumulative)
-typedef double SegsRate;  // Segs per second
+typedef double SegsRate;	  // Segs per second
 
 class SlowConv : public CCC {
    public:
 	struct History {
 		Time creation_tstamp;
 
-        TimeDelta interval_min_rtt;
+		TimeDelta interval_min_rtt;
 		TimeDelta interval_max_rtt;
 
 		SeqNum creation_cum_sent_segs;
-        SeqNum creation_cum_delivered_segs;
+		SeqNum creation_cum_delivered_segs;
 		SeqNum creation_cum_lost_segs;
 
 		SegsRate creation_sending_rate;
 
-        // What was delivered when the packet corresponding to this ACK was sent.
+		// What was delivered when the packet corresponding to this ACK was
+		// sent.
 		SeqNum creation_cum_segs_delivered_at_send;
 
 		SeqNumDelta interval_segs_lost;
@@ -32,30 +33,26 @@ class SlowConv : public CCC {
 	};
 
 	struct Beliefs {
-        // Updated on every ACK
-        TimeDelta min_rtt;
+		// Updated on every ACK
+		TimeDelta min_rtt;
 		TimeDelta min_qdel;
-		SeqNumDelta bq_belief1;  // inflight segs
+		SeqNumDelta bq_belief1;	 // inflight segs
 
-        // Updated every rtprop time (on seg sent)
-        SegsRate min_c_lambda;
+		// Updated every rtprop time (on seg sent)
+		SegsRate min_c_lambda;
 		SegsRate last_min_c_lambda;
-        SeqNumDelta bq_belief2;  // estimated bottleneck queue segs
-        SeqNum last_segs_sent;
+		SeqNumDelta bq_belief2;	 // estimated bottleneck queue segs
+		SeqNum last_segs_sent;
 	};
 
-	enum State {
-		SLOW_START,
-		PROBE,
-		DRAIN
-	};
+	enum State { SLOW_START, PROBE, DRAIN };
 
 	struct SegmentData {
 		// On send
-        Time send_tstamp;
+		Time send_tstamp;
 		SeqNum cum_segs_delivered_at_send;
 
-        // On ACK
+		// On ACK
 		TimeDelta rtt;
 		SeqNumDelta this_loss_count;
 	};
@@ -63,14 +60,14 @@ class SlowConv : public CCC {
 	static const int HISTORY_SIZE = 32;
 	static const SeqNumDelta MIN_CWND = 5;
 	static const SegsRate INIT_MIN_C = MIN_CWND;  // ~60 Kbps
-    static const SegsRate INIT_MAX_C = 1e5;  // ~1.2 Gbps
+	static const SegsRate INIT_MAX_C = 1e5;		  // ~1.2 Gbps
 	static const TimeDelta MS_TO_SECS = 1e3;
-	static const double INTER_HISTORY_TIME = 1;	 // Multiple of min_rtt
-    static const double BELIEFS_TIMEOUT_PERIOD = 1;  // Multiple of min_rtt
-    static const double JITTER_MULTIPLIER = 1; // Multiple of min_rtt
-    static const int MEASUREMENT_INTERVAL_RTT = 1;  // Multiple of min_rtt
+	static const double INTER_HISTORY_TIME = 1;		 // Multiple of min_rtt
+	static const double BELIEFS_TIMEOUT_PERIOD = 1;	 // Multiple of min_rtt
+	static const double JITTER_MULTIPLIER = 1;		 // Multiple of min_rtt
+	static const int MEASUREMENT_INTERVAL_RTT = 1;	 // Multiple of min_rtt
 	static const int MEASUREMENT_INTERVAL_HISTORY =
-		MEASUREMENT_INTERVAL_RTT / INTER_HISTORY_TIME;  // Multiple of history
+		MEASUREMENT_INTERVAL_RTT / INTER_HISTORY_TIME;	// Multiple of history
 
    protected:
 	Time cur_tick;
@@ -86,12 +83,12 @@ class SlowConv : public CCC {
 	boost::circular_buffer<History> history;
 	Beliefs beliefs;
 
-    SeqNum cum_segs_sent;
-    SeqNum cum_segs_delivered;
-    SeqNum cum_segs_lost;
-    SegsRate sending_rate;
+	SeqNum cum_segs_sent;
+	SeqNum cum_segs_delivered;
+	SeqNum cum_segs_lost;
+	SegsRate sending_rate;
 
-    Time current_timestamp() { return cur_tick; }
+	Time current_timestamp() { return cur_tick; }
 	void update_beliefs_minc_maxc(Time now);
 	void update_beliefs_minc_lambda(Time now);
 	void update_beliefs(Time, SegmentData, bool, TimeDelta);
